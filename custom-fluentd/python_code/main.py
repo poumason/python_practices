c936b2aa027b852c76032f847d079ccf88fb1b47
poulin <poumason@live.com>
2024-07-29 23:29:21 +0800

    add test code

--- custom-fluentd/python_code/main.py
+++ custom-fluentd/python_code/main.py
@@ -1,41 +1,37 @@
-import datetime
+
 import glob
-import json
 
-def get_log_file(date):
-    return "log_{}.txt".format(date.strftime("%Y-%m-%d"))
+import os
+from datetime import datetime
 
-last_line = None
+# Directory where the log files are located
+log_directory = os.path.join(os.getcwd(), 'custom-fluentd/logs/')
 
-def read_log():
-    pattern = "log_{}.txt"
-    for filename in glob.iglob(pattern):
-        with open(filename, 'r') as f:
-            parsed_data = []
-            for line in f:
-                # Split the line into its components
-                date, level, logger, message = line.split("|")
 
-                # Create a dictionary with the parsed data
-                entry = {
-                    "date": date,
-                    "level": level,
-                    "logger": logger,
-                    "message": message
-                }
+def process_log(filename):
+    # Your custom logic for processing the log file goes here.
+    # For example, reading and doing something with the content of the file.
+    print("Processing:", filename)
 
-                # Add the dictionary to the list of parsed data
-                parsed_data.append(entry)
 
-                # Convert the list of dictionaries into a JSON string
-                json_string = json.dumps(parsed_data)
 
-                # Print the JSON string
-                print(json_string)
+today=datetime.now().strftime('%Y-%m-%d')
+# Get all the log files in the specified directory using glob pattern
+file_pattern = os.path.join(log_directory, f'nlog-transcation-{today}*.log')
+print(os.listdir(log_directory))
+files = glob.glob(file_pattern)
 
-                # Add the dictionary to the list of parsed data if it has changed since the last read line
-                if entry != last_line:
-                    parsed_data.append(entry)
+# Sort the files based on their names (latest first)
+# files.sort(key=lambda x: int(os.path.splitext(x)[0].split('-')[-1][1:]))
+files.sort()
 
-if __name__ == '__main__':
-    print('start')
+process_status  = []
+for item in files:
+    file_name = os.path.basename(item)
+    print(f"Processing {file_name}")
+
+for file in reversed(files):
+    process_log(file)
+# if __name__ == '__main__':
+#     print('start')
+#     read_log()
