bc5234ee128ecc7a3e78759c3fbc0e36969bc160
poulin <poumason@live.com>
2026-06-25 23:49:46 +0800

    fixed mcp

--- mcp/server/line_server_auth.py
+++ mcp/server/line_server_auth.py
@@ -3,22 +3,31 @@ from linebot import (LineBotApi)
 from linebot.models import (TextSendMessage)
 import os
 import dotenv
-from fastmcp.server.auth import BearerAuthProvider, OAuth
+
+import json
+# from fastmcp.server.auth import BearerAuthProvider, OAuth
 
 dotenv.load_dotenv()
-
-auth = BearerAuthProvider(
-
-)
-
+print(os.getenv('LINE_TOKEN'))
 
 # Create a server instance
-mcp = FastMCP(name="MyAssistantServer", auth=auth)
+mcp = FastMCP(name="assistant_server")
 
-@mcp.tool
-def multiply(a: float, b: float) -> float:
-    """Multiplies two numbers."""
-    return a * b
+# Basic dynamic resource returning a string
+@mcp.resource("resource://greeting")
+def get_greeting() -> str:
+    """Provides a simple greeting message."""
+    return "Hello from FastMCP Resources!"
+
+# Resource returning JSON data
+@mcp.resource("data://config")
+def get_config() -> str:
+    """Provides application configuration as JSON."""
+    return json.dumps({
+        "theme": "dark",
+        "version": "1.2.0",
+        "features": ["tools", "send_message"],
+    })
 
 @mcp.tool
 async def send_message(message: str):
@@ -28,13 +37,15 @@ async def send_message(message: str):
     Args:
         message: text content.
     """
+    print(message)
     token = os.getenv('LINE_TOKEN')
     user = os.getenv('LINE_ROOM')
     line_bot_api = LineBotApi(token)
     line_bot_api.push_message(user, TextSendMessage(text=message))
     return "ok"
 
+
 if __name__ == "__main__":
-    mcp.run(transport="sse",
+    mcp.run(transport="streamable-http",
             host='0.0.0.0',
             port=8788)
